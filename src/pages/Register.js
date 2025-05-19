7afa95ff95f5f17d8a72b56f921d1bc29ef86970
shthomas1 <142743785+shthomas1@users.noreply.github.com>
2025-05-19 18:10:37 -0500

    V.02

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "../styles/pages/Register.css";
 import { FaGithub } from "react-icons/fa";
-import React, { useState, useEffect, useCallback } from "react";
 
 const Register = () => {
   const [formData, setFormData] = useState({
