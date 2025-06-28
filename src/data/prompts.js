b971c97b12ea34bf9a5259a41685a5c7de6a9743
shthomas1 <142743785+shthomas1@users.noreply.github.com>
2025-06-28 05:56:50 -0500

    V1.50

--- src/data/prompts.js
+++ src/data/prompts.js
@@ -5,27 +5,27 @@ export const promptsData = [
     title: 'Cybersecurity Challenge',
     description: 'Real-world cybersecurity challenge focused on human security factors!',
     difficulty: '3/5',
-    driveLink: 'https://drive.google.com/placeholder'
+    driveLink: 'https://docs.google.com/document/d/1POR4juJpltkwaU8nonb0Xi3ozZiLtj42hLnyBFSDMpA/edit?usp=sharing'
   },
   {
     id: 2,
     title: 'Data Analytics/Data Science Challenge',
     description: 'Manufacturing-focused data analytics project!',
     difficulty: '4/5',
-    driveLink: 'https://drive.google.com/placeholder'
+    driveLink: 'https://docs.google.com/document/d/1H4kwBKyE8NyNHxszD9AWe1UbYiAzSVfSDZpBqlFERH8/edit?usp=sharing'
   },
   {
     id: 3,
     title: 'Full-Stack Challenge',
     description: 'Startup-focused full-stack web application!',
     difficulty: '4/5',
-    driveLink: 'https://drive.google.com/placeholder'
+    driveLink: 'https://docs.google.com/document/d/139wVbcnwmo-xJv2pZ2PrsRgIcFosW80vuRylE_NqQTQ/edit?usp=sharing'
   },
   {
     id: 4,
     title: 'Game Development Challenge',
     description: "Can't be too specific about this one!",
     difficulty: '1-5/5',
-    driveLink: 'https://drive.google.com/placeholder'
+    driveLink: 'https://docs.google.com/document/d/1vuvpRF9uSXZYjAAOWs10cy7v_UxuPye6kwS0EFAcxUE/edit?usp=sharing'
   },
 ];
